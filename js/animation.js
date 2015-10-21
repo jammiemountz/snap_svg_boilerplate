c341dd68911585f37635a6ecb47f56fcf304450b
Jammie Mountz <jmmountz@gmail.com>
2015-10-21 11:44:45 -0700

    put in svg, attempted animation

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,10 @@
 console.log('animate your svgs here')
 
 var mysvg = Snap('#mysvg')
+
+mysvg.selectAll('path').forEach(function(el){
+  el.animate({
+    left: (Math.random() * 5),
+    top: (Math.random() * 5)
+  }, 1000)
+})
